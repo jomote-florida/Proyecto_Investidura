fbb78e6c4caf2d396b013d018be562a9a025eb47
jomote-florida <jomote@alumnatflorida.es>
2025-03-06 18:10:15 +0100

    Actualización de organizacion

--- src/routes/routesindex.js
+++ src/routes/routesindex.js
@@ -3,22 +3,21 @@ const router = Router();
 
 
 
-router.post("/rutaPost", (req,res) =>{
+router.post("/rutaPost", (req,res) =>{//Esta ruta nos mostrará en consola los datos recibidos por el formulario
     console.log("Enviada peticion POST");
-    // res.send("Datos recibidos de un post<br>");
     console.log(req.body);
-    res.json({
+    res.json({//Y le mostrará al usuario un archivo json con la información introducida
         nombre: req.body.nombre,
         apellido: req.body.apellido,
         investidura: req.body.investidura,
         libroLeido : req.body.radio,
-        contraseña : "****",
+        contraseña : "****",//Menos la contraseña que se pondrá con asteriscos
 
     });
 })
 
 router.post("/informacionFormulario", (req, res) => {
-    console.log("Enviada petición Post");
+    console.log("Enviada petición Post");//Esta en cambio lo que manda es al usuario a otra pagina web, pero si muestra en consola los datos introducidos
     console.log(req.body);
     res.redirect("http://127.0.0.1:5500/src/front/roshar2.html");
 })
