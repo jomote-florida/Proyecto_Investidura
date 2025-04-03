dee05d8a59ea3f2ff789cc19881f6046d68b4f93
jomote-florida <jomote@alumnatflorida.es>
2025-04-03 20:33:53 +0200

    nuesvas rutas post para AP8

--- src/routes/routesindex.js
+++ src/routes/routesindex.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
+//Rutas get
 router.get("/rutaGetPruebaUnity", (req,res) =>{
     console.log("Entrada a ruta Get");
     res.send("Hola desde el get de prueba para Unity");
@@ -26,6 +27,7 @@ router.get("/ultimate", (req,res) =>{
     res.send("... que el texto apareciera en pantalla en vez en consola");
 })
 
+//Rutas POST
 router.post("/rutaPost", (req,res) =>{//Esta ruta nos mostrará en consola los datos recibidos por el formulario
     console.log("Enviada peticion POST");
     console.log(req.body);
@@ -43,6 +45,24 @@ router.post("/informacionFormulario", (req, res) => {
     console.log("Enviada petición Post");//Esta en cambio lo que manda es al usuario a otra pagina web, pero si muestra en consola los datos introducidos
     console.log(req.body);
     res.redirect("https://jomote-florida.github.io/Proyecto_Investidura_Frontend/roshar2.html");
+})
+
+router.post("/rutaPost3", (req,res)=>{
+    console.log("Seguimos avanzando en proyecto");
+    console.log(req.body);
+    res.send("Has entrado en el post");
+})
+
+router.post(("yOtroMas", (req,res){
+    console.log("Si tu quisieras");
+    console.log(req.body);
+    res.send("Estarias en pijama");
+}))
+
+router.post("/yaLaUltima",(req,res)=>{
+    console.log("SACABO");
+    console.log(req.body);
+    res.send("La verdad es que no recuerdo porque ponía req.body");
 })
 
 module.exports = router;
