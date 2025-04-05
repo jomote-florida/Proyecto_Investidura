44269a24a1927373b8b2b85011d0226224ac8ff2
jomote-florida <jomote@alumnatflorida.es>
2025-04-05 23:31:06 +0200

    correccion de un parentesis?

--- src/routes/routesindex.js
+++ src/routes/routesindex.js
@@ -54,11 +54,11 @@ router.post("/rutaPost3", (req,res)=>{
     res.send("Has entrado en el post");
 })
 
-router.post(("/yOtroMas", (req,res)=>{
+router.post("/yOtroMas", (req,res)=>{
     console.log("Si tu quisieras");
     console.log(req.body);
     res.send("Estarias en pijama");
-}))
+})
 
 router.post("/yaLaUltima",(req,res)=>{
     console.log("SACABO");
